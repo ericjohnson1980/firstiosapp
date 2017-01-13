51639cb3aec5d3f8c4ad74bd9e4f5ab783249f2a
junjie.hu.ataway@gmail.com <junjie.hu.ataway@gmail.com>
2017-01-13 22:58:35 +0800

    First code commit

---
+++ firstiosapp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  firstiosapp
+//
+//  Created by Hu, Junjie on 1/13/17.
+//  Copyright © 2017 Hu, Junjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+            {
+        
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
